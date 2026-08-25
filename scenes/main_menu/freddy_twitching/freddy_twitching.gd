da22cbfc1628a46016b56bebcc099878e6b8b53c
LOUDO <lucaslelouer@gmail.com>
2026-08-25 17:24:59 +0200

    fix: bad timer freddy twitching

--- scenes/main_menu/freddy_twitching/freddy_twitching.gd
+++ scenes/main_menu/freddy_twitching/freddy_twitching.gd
@@ -1,22 +1,9 @@
 extends AnimatedSprite2D
 
-const FLICKER := 0.8
-
 @onready var variant_timer := $"Variant Timer"
-
-var current_flicker := 0.0
-
-func _process(delta: float) -> void:
-	current_flicker += delta
-	if current_flicker >= FLICKER:
-		current_flicker = 0.0
-		frame = 0
-		var random_pose = randi_range(0, 99)
-		if random_pose >= 1 and random_pose <= 3:
-			variant_timer.start()
-			frame = random_pose
-			
-
 
 func _on_variant_timer_timeout() -> void:
 	frame = 0
+	var random_pose = randi_range(0, 99)
+	if random_pose >= 1 and random_pose <= 3:
+		frame = random_pose
